26259f65db45a736ede4c2444fd14341cce89b1b
RohRohRaggy <Max.nikoliszyn@student.gildeopleidingen.nl>
2026-05-20 10:10:44 +0200

    Update
    - Added v2 back from temp move.
    - removed v1 after making renewed release from feedback.

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 //span elements
 const humanOutput = document.querySelector("#human");
+const humanWins = document.querySelector("#HWins");
+const humanLosses = document.querySelector("#HLosses");
 const computerOutput = document.querySelector("#computer");
+const computerWins = document.querySelector("#CPUWins");
+const computerLosses = document.querySelector("#CPULosses");
 const resultOutput = document.querySelector("#result");
 
+let hWinsCount = 0;
+let hLossesCount = 0;
+let cpuWinsCount = 0;
+let cpuLossesCount = 0;
 let humanChoice = "";
 let computerChoice = "";
 
+humanWins.innerHTML = hWinsCount;
+humanLosses.innerHTML = hLossesCount;
+computerWins.innerHTML = cpuWinsCount;
+computerLosses.innerHTML = cpuLossesCount;
 humanOutput.innerHTML = "Make your choice";
 computerOutput.innerHTML = "Deciding...";
 resultOutput.innerHTML = "Result";
+
+//play
+function play(event) {
+    humanChoice = event.target.id;
+    humanOutput.innerHTML = humanChoice;
+    rdmnum();
+    computerOutput.innerHTML = computerChoice;
+    getResult();
+}
+//random number function toegevoegd om herhaling te vermijden
+function rdmnum() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch (randomNumber) { //if statements vervangen door switch case
+        case 1:
+            computerChoice = 'rock';
+            break;
+        case 2:
+            computerChoice = 'scissors';
+            break;
+        case 3:
+            computerChoice = 'paper';
+            break;
+        default:
+
+    }
+}
 
 //RPS result
 function getResult() {
@@ -21,62 +59,21 @@ function getResult() {
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
         resultOutput.innerHTML = "You win!";
+        hWinsCount++;
+        cpuLossesCount++;
     } else {
         resultOutput.innerHTML = "CPU wins!";
+        hLossesCount++;
+        cpuWinsCount++;
     }
+    humanWins.innerHTML = hWinsCount;
+    humanLosses.innerHTML = hLossesCount;
+    computerWins.innerHTML = cpuWinsCount;
+    computerLosses.innerHTML = cpuLossesCount;
 }
 
-//Button click event
-const rock = document.querySelector("#rock");
-rock.addEventListener("click", function (event) {
-    humanChoice = event.target.id;
-    humanOutput.innerHTML = humanChoice;
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
-        computerChoice = 'rock';
-    }
-    if (randomNumber === 2) {
-        computerChoice = 'scissors';
-    }
-    if (randomNumber === 3) {
-        computerChoice = 'paper';
-    }
-    computerOutput.innerHTML = computerChoice;
-    getResult();
-});
-
-const paper = document.querySelector("#paper");
-paper.addEventListener("click", function (event) {
-    humanChoice = event.target.id;
-    humanOutput.innerHTML = humanChoice;
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
-        computerChoice = 'rock';
-    }
-    if (randomNumber === 2) {
-        computerChoice = 'scissors';
-    }
-    if (randomNumber === 3) {
-        computerChoice = 'paper';
-    }
-    computerOutput.innerHTML = computerChoice;
-    getResult();
-});
-
-const scissors = document.querySelector("#scissors");
-scissors.addEventListener("click", function (event) {
-    humanChoice = event.target.id;
-    humanOutput.innerHTML = humanChoice;
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-    if (randomNumber === 1) {
-        computerChoice = 'rock';
-    }
-    if (randomNumber === 2) {
-        computerChoice = 'scissors';
-    }
-    if (randomNumber === 3) {
-        computerChoice = 'paper';
-    }
-    computerOutput.innerHTML = computerChoice;
-    getResult();
+//Button click event ingekort
+const btns = document.querySelectorAll("button");
+btns.forEach(btn => {
+    btn.addEventListener("click", play);
 });
